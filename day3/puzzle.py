14ce5964661910be81051af5caadf016195d7136
Nikola Dragovic <nikoladragovic@hotmail.com>
2020-12-03 10:59:06 +0100

    refactor solution for day 3

--- day3/puzzle.py
+++ day3/puzzle.py
@@ -1,37 +1,27 @@
 # AOC day 3
 # puzzle 1
+from functools import reduce
+
+
+# count trees in a path with slope down/right
+def count_trees(lines, right, down):
+    indices = [int((right * i) / down) % len(lines[0]) if i % down == 0 else -1 for i in range(0, len(lines))]
+    return sum([x[i] == '#' if i != -1 else False for (x, i) in zip(lines, indices)])
+
 
 # read input
 with open("input.txt") as f:
     entries = [x.strip('\n') for x in f.readlines()]
 
-# compute relevant indices
 # right 3, down 1
-indices = [(3*i) % len(entries[0]) for i in range(0, len(entries))]
-
-# check for trees at index, entry
-rl = [x[i] == '#' for (x, i) in zip(entries, indices)]
-
-# print result
-print(sum(rl))
+rl = count_trees(entries, 3, 1)
+print(rl)
 
 # puzzle 2
+slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
 
-# right 1, down 1
-indices = [i % len(entries[0]) for i in range(0, len(entries))]
-rl1 = [x[i] == '#' for (x, i) in zip(entries, indices)]
-
-# right 5, down 1
-indices = [(5 * i) % len(entries[0]) for i in range(0, len(entries))]
-rl2 = [x[i] == '#' for (x, i) in zip(entries, indices)]
-
-# right 7, down 1
-indices = [(7 * i) % len(entries[0]) for i in range(0, len(entries))]
-rl3 = [x[i] == '#' for (x, i) in zip(entries, indices)]
-
-# right 1, down 2
-indices = [int(i/2) % len(entries[0]) if i % 2 == 0 else -1 for i in range(0, len(entries))]
-rl4 = [x[i] == '#' if i != -1 else False for (x, i) in zip(entries, indices)]
+# count trees for each slope
+rl = [count_trees(entries, *s) for s in slopes]
 
 # print result
-print(sum(rl) * sum(rl1) * sum(rl2) * sum(rl3) * sum(rl4))
+print(reduce(lambda x, y: x * y, rl))
